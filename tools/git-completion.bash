c6692a2b53e444d8d32ecd67d1d28050d06fdd77
Victor Yarema <victor.yarema@gmail.com>
2022-08-11 12:46:36 +0200

    Temporary hotfix.

--- tools/git-completion.bash
+++ tools/git-completion.bash
@@ -3518,12 +3518,13 @@ __git_func_wrap ()
 }
 
 ___git_complete ()
-{
+(
+	set +u
 	local wrapper="__git_wrap${2}"
 	eval "$wrapper () { __git_func_wrap $2 ; }"
 	complete -o bashdefault -o default -o nospace -F $wrapper $1 2>/dev/null \
 		|| complete -o default -o nospace -F $wrapper $1
-}
+)
 
 # Setup the completion for git commands
 # 1: command or alias
